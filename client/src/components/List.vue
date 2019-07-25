544db2bd7f4a5a11e8ba810e3a5a48e6d77f7893
LynneMartin <lynnemartin@gmail.com>
2019-07-25 14:16:02 -0600

    testing add task form

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -3,9 +3,17 @@
     <h3>Title: {{listProp.title}}</h3>
     <p>Description: {{listProp.description}}</p>
 
+    <div>
+    <form class="addTaskForm" @submit.prevent="addTask" id="addTaskForm">
+      <input v-model="task.title" type="text" class="form-control formBox" id="task-title" placeholder="New Task">
+      <button type="submit" class="btn">
+        <i class="fas fa-plus-circle"></i>
+      </button>
+    </form>
+  </div>
     <!-- TODO Add Task form will go here?We want it to be on the same List card above or next to Delete button-->
-  
-  <button class="mt-auto btn btn-danger btn-sm mb-2" @click="deleteList(listProp)"><i class="fa fa-trash"></i></button>
+    
+    <button class="mt-auto btn btn-danger btn-sm mb-2" @click="deleteList(listProp)"><i class="fa fa-trash"></i></button>
 </div>
 </template>
 
@@ -15,13 +23,13 @@
     name: 'Lists',
     props: ['listProp'],
     mounted() {
-      this.$store.dispatch('getListsByBoard', this.list._id)
+      this.$store.dispatch('getListsByBoard', this.listProp._id)
     },
     data() {
       return {
         task: {
-          boardId: this.list.boardId,
-          listId: this.list._id
+          boardId: this.listProp.boardId,
+          listId: this.listProp._id
         }
       }
     },
